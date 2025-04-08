1f1b018654d8747adc46c09ec53b4943a7735546
dsaadatmandi <davidmilad@gmail.com>
2025-04-08 23:56:04 +0200

    add example usage

--- examples/run.py
+++ examples/run.py
@@ -1,23 +1,17 @@
-from typing import Optional
-from warnings import catch_warnings
-from RPLidarC1 import RPLidar
+from scanner import RPLidar
 import asyncio
 import logging
 import time
 
 
 async def main(lidar: RPLidar):
-    # lidar.healthcheck()
-    lidar.start_scan()
-    # lidar.get_info()
-    print(lidar.coro_list)
     async with asyncio.TaskGroup() as tg:
         tg.create_task(wait_and_stop(5, lidar.stop_event))
         tg.create_task(queue_printer(lidar.output_queue, lidar.stop_event))
-        for coro in lidar.coro_list:
-            tg.create_task(coro)
+        tg.create_task(lidar.simple_scan(make_return_dict=True))
+
     print(lidar.output_dict)
-    reset(lidar)
+    lidar.reset()
 
 
 async def wait_and_stop(t, event: asyncio.Event):
@@ -37,13 +31,6 @@ async def queue_printer(q: asyncio.Queue, event: asyncio.Event):
         print(out)
 
 
-def reset(lidar: RPLidar):
-    print("Resetting before shutdown.")
-    lidar.clear_input_buffer()
-    lidar._send_command(lidar.RESET_BYTE)
-    lidar.disconnect()
-
-
 if __name__ == "__main__":
     logging.basicConfig(level=0)
     lidar = RPLidar("/dev/ttyUSB0", 460800)
@@ -52,4 +39,4 @@ if __name__ == "__main__":
         asyncio.run(main(lidar))
     except KeyboardInterrupt:
         time.sleep(1)
-        reset(lidar)
+        lidar.reset()
